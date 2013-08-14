20b106fb5bb69fbf5e8765e9aebeb64dda65c00c
Rob Reynolds <ferventcoder@gmail.com>
2013-08-14 01:41:44 -0500

    Set public once released

--- baseboxes/win7pro-x64-vbox4216-nocm/release.sh
+++ baseboxes/win7pro-x64-vbox4216-nocm/release.sh
@@ -5,3 +5,4 @@ BoxName=${BoxBaseName}.box
 s3cmd put $BoxName $S3BaseLocation/${BoxName}.upload
 s3cmd del $S3BaseLocation/$BoxName
 s3cmd mv $S3BaseLocation/${BoxName}.upload $S3BaseLocation/$BoxName
+s3cmd setacl —-acl-public $S3BaseLocation/$BoxName
