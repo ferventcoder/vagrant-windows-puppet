6280e21a4c658c779471db0b681d53c0899667c5
Rob Reynolds <ferventcoder@gmail.com>
2013-08-18 09:18:17 -0500

    Don't set acl public by default
    The s3cmd has issues running this, it could be a 
    timing issue and may need to just chill for a 
    second prior to trying to run the command.

--- baseboxes/win7pro-x64-vbox4216-nocm/release.sh
+++ baseboxes/win7pro-x64-vbox4216-nocm/release.sh
@@ -7,4 +7,4 @@ BoxName=${BoxBaseName}.box
 s3cmd put $BoxName $S3BaseLocation/${BoxName}.upload
 s3cmd del $S3BaseLocation/$BoxName
 s3cmd mv $S3BaseLocation/${BoxName}.upload $S3BaseLocation/$BoxName
-s3cmd setacl —-acl-public $S3BaseLocation/$BoxName
+#s3cmd setacl —-acl-public $S3BaseLocation/$BoxName
