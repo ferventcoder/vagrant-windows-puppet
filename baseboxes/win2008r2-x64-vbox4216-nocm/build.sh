c8d0af9ffe62651cb71851b967a49eeca85f4e88
Rob Reynolds <ferventcoder@gmail.com>
2013-08-18 22:44:46 -0500

    Win2008 R2 x64 Basebox

---
+++ baseboxes/win2008r2-x64-vbox4216-nocm/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CurrentDir=$(pwd)
+BoxBaseName=${PWD##*/}
+VMName=win2008r2x64
+VMPath=~/VirtualBox\ VMs/${VMName}/${VMName}.vbox
+BoxName=${BoxBaseName}.box
+OVFName=box.ovf
+VagrantBoxName=${VMName}
+
+printf "Creating a vagrant basebox named $BoxName \n"
+
+# Remove existing artifacts
+rm -f box-disk1.vmdk
+rm -f box.ovf
+rm -f $BoxName
+
+# Prepare the VM
+vboxmanage export "$VMPath" -o $OVFName #--vsys 0 --eula "This is for evaluation purposes only. You must provide a valid license to use this box."
+
+# Tar and gzip the box
+tar --exclude='*.sh' -cvzf $BoxName ./*
+
+# Remove the artifacts to save space
+rm -f box-disk1.vmdk
+rm -f box.ovf
+
+# Add/update the box
+vagrant box add "$VagrantBoxName" $BoxName --provider virtualbox --force
