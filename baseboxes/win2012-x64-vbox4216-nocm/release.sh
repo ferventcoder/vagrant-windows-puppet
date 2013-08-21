ad2167ace8b843b91c21ee0e89c1bcade8b6f561
Rob Reynolds <ferventcoder@gmail.com>
2013-08-21 15:16:45 -0700

    Win2012 base box

---
+++ baseboxes/win2012-x64-vbox4216-nocm/release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+S3BaseLocation=s3://vagrant-windows
+BoxBaseName=${PWD##*/}
+BoxName=${BoxBaseName}.box
+
+s3cmd put $BoxName $S3BaseLocation/${BoxName}.upload --force
+s3cmd del $S3BaseLocation/$BoxName
+s3cmd mv $S3BaseLocation/${BoxName}.upload $S3BaseLocation/$BoxName
+sleep 1
+s3cmd setacl —-acl-public $S3BaseLocation/$BoxName
