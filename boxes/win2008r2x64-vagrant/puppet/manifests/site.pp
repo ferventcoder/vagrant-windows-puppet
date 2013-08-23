81f3a0e0bdcbaceea580dd746d438fe6f1a55374
Rob Reynolds <ferventcoder@gmail.com>
2013-08-23 08:13:55 -0700

    updating the site manifest

--- boxes/win2008r2x64-vagrant/puppet/manifests/site.pp
+++ boxes/win2008r2x64-vagrant/puppet/manifests/site.pp
@@ -3,6 +3,28 @@
 package {'git.install':
   ensure => '1.8.1.2',
   provider => 'chocolatey',
-  #install_options => '-source c:\vagrant\resources\packages',
+  #install_options => '',
   source => 'c:\vagrant\resources\packages',
+}
+
+# package {'git.install':
+#   ensure => latest,
+#   provider => 'chocolatey',
+#   source => 'c:\vagrant\resources\packages',
+# }
+
+ package {'poshgit':
+  ensure => latest,
+  provider => 'chocolatey',
+  source => 'c:\vagrant\resources\packages',
+}
+
+# package {'putty':
+#   ensure => latest,
+#   provider => 'chocolatey',
+# }
+
+package {'putty':
+  ensure => absent,
+  provider => 'chocolatey',
 }
