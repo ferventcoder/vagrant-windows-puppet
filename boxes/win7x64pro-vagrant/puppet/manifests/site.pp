ac345be5a3453054bb19a5139955514c6b52d326
Rob Reynolds <ferventcoder@gmail.com>
2013-08-24 12:32:17 -0700

    Adding site.pp package notes

--- boxes/win7x64pro-vagrant/puppet/manifests/site.pp
+++ boxes/win7x64pro-vagrant/puppet/manifests/site.pp
@@ -1,17 +1,24 @@
 
-
-package {'git.install':
-  ensure => '1.8.1.2',
-  provider => 'chocolatey',
-  #install_options => '',
-  source => 'c:\vagrant\resources\packages',
-}
-
-# package {'git.install':
-#   ensure => latest,
+# package {'name_of_package':
 #   provider => 'chocolatey',
+#   ensure => absent,latest, '1.0',
+#   source => 'http://not/default/sources;c:\local;\\some\network\share',
+#   install_options => '-installArgs "addtl args for native installer"',
+# }
+
+
+# package {'git':
+#   ensure => '1.8.1.2',
+#   provider => 'chocolatey',
+#   #install_options => '',
 #   source => 'c:\vagrant\resources\packages',
 # }
+
+package {'git.install':
+  ensure => latest,
+  provider => 'chocolatey',
+  source => 'c:\vagrant\resources\packages',
+}
 
  package {'poshgit':
   ensure => latest,
