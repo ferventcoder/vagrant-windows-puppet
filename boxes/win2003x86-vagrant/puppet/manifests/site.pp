79e90edffba0ecda5c612c840db75ec34759c0da
Rob Reynolds <ferventcoder@gmail.com>
2013-08-28 13:18:31 -0500

    updating 2003 box stuffs

---
+++ boxes/win2003x86-vagrant/puppet/manifests/site.pp
@@ -0,0 +1,27 @@
+file { 'c:/before.txt':
+  content   => 'one',
+  before    => File['c:/after.txt']
+}
+
+reboot { 'reboot_now':
+  message => "Rebooting yo",
+  timeout => 2,
+
+}
+
+reboot { 'reboot_pending':
+  message => "Rebooting pending",
+  when => pending,
+  timeout => 2,
+
+}
+
+file { 'c:/after.txt':
+  content   => 'two',
+  notify => Reboot['reboot_now']
+}
+
+file { 'c:/afterreboot.txt':
+  content => 'yep',
+  #subscribe => Reboot['reboot_now']
+}
