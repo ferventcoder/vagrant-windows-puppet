44ff34dd662c20708a46687f3be49a1e37d9a6b4
Rob Reynolds <ferventcoder@gmail.com>
2013-09-18 14:58:08 -0500

    failed attempt to get VagrantWindows included in vagrantfile

---
+++ boxes/VagrantWindows.rb
@@ -0,0 +1,49 @@
+Vagrant.configure("2") do |config|
+  # Windows specific configuration done here
+  config.vm.provider :virtualbox do |v|
+    v.gui = true
+  end
+
+  config.vm.provider :vmware_fusion do |v|
+    v.gui = true
+  end
+
+  # Create a private network, which allows host-only access to the machine
+  # using a specific IP.
+  # config.vm.network :private_network, ip: "192.168.33.10"
+
+
+  # Max time to wait for the guest to shutdown
+  config.windows.halt_timeout = 20
+
+  # Admin user name and password
+  config.winrm.username = "vagrant"
+  config.winrm.password = "vagrant"
+
+  # Configure base box parameters
+  config.vm.guest = :windows
+
+  # Port forward WinRM and RDP
+  #config.vm.network :forwarded_port, guest: 3389, host: 3389
+  config.vm.network :forwarded_port, guest: 5985, host: 5985
+
+  # config.vm.synced_folder "~", "/home"
+  # config.vm.synced_folder "puppet/box_modules", "/ProgramData/PuppetLabs/puppet/etc/modules"
+
+  # # This shell provisioner installs chocolatey, ruby, and puppet. Also runs librarian-puppet.
+  # config.vm.provision :shell, :path => "shell/main.cmd"
+
+  # # Enable provisioning with Puppet stand alone.  Puppet manifests
+  # # are contained in a directory path relative to this Vagrantfile.
+  # config.vm.provision :puppet, :options => ["--debug --trace --verbose"] do |puppet|
+  #   puppet.manifests_path = "puppet/manifests"
+  #   puppet.manifest_file  = "site.pp"
+  #   puppet.module_path = "puppet/modules"
+  #   puppet.facter = {
+  #     "domain" => "local",
+  #     "kernel" => "windows",
+  #     "operatingsystem" => "windows",
+  #     "osfamily" => "windows",
+  #   }
+  # end
+end
