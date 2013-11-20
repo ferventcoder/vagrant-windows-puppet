12f90385b467e3be782a90780dab4155d6258555
Rob Reynolds <ferventcoder@gmail.com>
2013-11-20 15:43:56 -0600

    Windows 2003 box is now also connecting to master and running.

--- boxes/centos-6.4-x64/puppet/manifests/site.pp
+++ boxes/centos-6.4-x64/puppet/manifests/site.pp
@@ -1,17 +1,58 @@
+node default {
+
+}
+
+node /^win2003.*$/ {
+
   file { 'c:/temp':
     ensure => directory,
-    source_permissions => ignore,
   }
 
-  # file { 'c:/temp/test.txt':
-  #     ensure => file,
-  #     source => "puppet:///modules/permissionstest/test.txt",
-  # }
+  file { 'c:/temp/test.txt':
+    ensure => file,
+    source => "puppet:///modules/permissionstest/test.txt",
+  }
 
   file { 'c:/temp/test-ignored.txt':
-      ensure => file,
-      source_permissions => ignore,
-      source => "puppet:///modules/permissionstest/test.txt",
+    ensure => file,
+    source => "puppet:///modules/permissionstest/test.txt",
+  }
+
+  file {'c:/temp/test-content.txt':
+    ensure => file,
+    content => 'This is not managed by source.'
+  }
+
+  file { 'c:/temp/test-setmode.txt':
+    ensure => file,
+    mode => 0640,
+    owner => 'Administrators',
+    group => 'SYSTEM',
+    source => "puppet:///modules/permissionstest/test-setmode.txt",
+  }
+}
+
+node /^win7.*$/ {
+
+  file { 'c:/temp':
+    ensure => directory,
+  }
+
+  file { 'c:/temp/test.txt':
+    ensure => file,
+    source_permissions => ignore,
+    source => "puppet:///modules/permissionstest/test.txt",
+  }
+
+  file { 'c:/temp/test-ignored.txt':
+    ensure => file,
+    source_permissions => ignore,
+    source => "puppet:///modules/permissionstest/test.txt",
+  }
+
+  file {'c:/temp/test-content.txt':
+    ensure => file,
+    content => 'This is not managed by source.'
   }
 
   file { 'c:/temp/test-setmode.txt':
@@ -22,3 +63,4 @@
     source_permissions => ignore,
     source => "puppet:///modules/permissionstest/test-setmode.txt",
   }
+}
