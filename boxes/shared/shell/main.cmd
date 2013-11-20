1db91980942db4c325a30e31911042efcbd28951
Rob Reynolds <ferventcoder@gmail.com>
2013-11-20 07:18:45 -0600

    Install puppet directly from msiexec url call Set hosts file appropriately

--- boxes/shared/shell/main.cmd
+++ boxes/shared/shell/main.cmd
@@ -1,11 +1,28 @@
-echo 'Ensuring .NET 4.0 is installed'
-@powershell -NoProfile -ExecutionPolicy Bypass -File "c:\vagrantshared\shell\InstallNet4.ps1"
+::echo 'Ensuring .NET 4.0 is installed'
+::@powershell -NoProfile -ExecutionPolicy Bypass -File "c:\vagrantshared\shell\InstallNet4.ps1"
 
-echo 'Ensuring Chocolatey is Installed'
-@powershell -NoProfile -ExecutionPolicy Bypass -File "c:\vagrantshared\shell\InstallChocolatey.ps1"
+::echo 'Ensuring Chocolatey is Installed'
+::@powershell -NoProfile -ExecutionPolicy Bypass -File "c:\vagrantshared\shell\InstallChocolatey.ps1"
+
+echo 'Checking if puppet server has been added to hosts file'
+call ping puppet
+if %errorlevel% neq 0 goto addpuppethost
+goto continue
+
+:addpuppethost
+echo 'Adding puppet (192.168.0.2) to hosts file. This is the same private IP that the centos-6.4-x64 box uses.'
+echo 192.168.0.2 puppet >> %SystemDrive%\Windows\System32\Drivers\etc\hosts
+
+goto continue
+
+:continue
+echo 'Ensuring Time Service is on'
+net start w32time
+w32tm /resync
 
 echo 'Install puppet if missing'
-@powershell -NoProfile -ExecutionPolicy Bypass -File "c:\vagrantshared\shell\InstallPuppet.ps1"
+::@powershell -NoProfile -ExecutionPolicy Bypass -File "c:\vagrantshared\shell\InstallPuppet.ps1"
+start /wait msiexec /qn /i http://downloads.puppetlabs.com/windows/puppet-3.3.2.msi
 
 SET PATH=%PATH%;%SystemDrive%\Program Files (x86)\Puppet Labs\Puppet\bin;%SystemDrive%\Program Files\Puppet Labs\Puppet\bin;
 
@@ -15,3 +32,6 @@ SET FACTER_domain=local
 
 echo 'Install Required libraries for puppet if missing'
 @powershell -NoProfile -ExecutionPolicy Bypass -File "c:\vagrantshared\shell\PreparePuppetProvisioner.ps1"
+
+::puppet resource
+call puppet agent --test --debug --verbose
