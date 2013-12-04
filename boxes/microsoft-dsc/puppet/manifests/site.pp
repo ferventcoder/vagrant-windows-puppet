2fb590badea0166ea7b2dd665924124bdc7ba39f
Rob Reynolds <ferventcoder@gmail.com>
2013-12-03 23:14:30 -0600

    Adding DSC

---
+++ boxes/microsoft-dsc/puppet/manifests/site.pp
@@ -0,0 +1,63 @@
+# Package {
+#   ensure => latest,
+#   provider => chocolatey,
+# }
+
+# VS dev tools
+# package {['SQLManagementStudio'
+#     ,'SQLExpressTools'
+#     ,'VWD_RTW'
+#     ,'VWD2010SP1AzurePack'
+#     ,'MVC3'
+#     ,'MVC3Loc'
+#     ]:
+#   ensure => installed,
+#   provider => chocolatey,
+#   source => "webpi"
+# }
+
+# IIS
+# package {['ASPNET'
+#     ,'ASPNET_REGIIS'
+#     ,'DefaultDocument'
+#     ,'DynamicContentCompression'
+#     ,'HTTPRedirection'
+#     ,'IIS7_ExtensionLessURLS'
+#     ,'IISExpress'
+#     ,'IISExpress_7_5'
+#     ,'IISManagementConsole'
+#     ,'ISAPIExtensions'
+#     ,'NetExtensibility'
+#     ,'RequestFiltering'
+#     ,'StaticContent'
+#     ,'StaticContentCompression'
+#     ,'UrlRewrite2'
+#     ,'WindowsAuthentication'
+#     ]:
+#   ensure => installed,
+#   provider => chocolatey,
+#   source => "webpi"
+# }
+
+#,'PowerGuiVSX'
+
+package {['SublimeText2.app'
+    ,'notepadplusplus'
+    ,'fiddler'
+    ,'sysinternals'
+    ,'poshgit'
+    ,'gitextensions'
+    ,'growl'
+    ,'conemu'
+    ,'powergui'
+    ,'pester'
+    ,'psget'
+    ]:
+  ensure => latest,
+  provider => chocolatey,
+}
+
+package {'powershell4':
+  ensure => latest,
+  provider => chocolatey,
+}
---
+++ boxes/microsoft-dsc/shell/DSC.install.cmd
@@ -0,0 +1,2 @@
+echo 'Setting up DSC components'
+@powershell -NoProfile -ExecutionPolicy Bypass -File "c:\vagrant\shell\SetupDSCComponents.ps1"
