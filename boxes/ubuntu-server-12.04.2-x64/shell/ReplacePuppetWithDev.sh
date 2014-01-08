deda806a46c6c2575295dd1a0502f90e5f9324a0
Rob Reynolds <ferventcoder@gmail.com>
2014-01-08 14:06:51 -0600

    (maint) ubuntu updates

---
+++ boxes/ubuntu-server-12.04.2-x64/shell/ReplacePuppetWithDev.sh
@@ -0,0 +1,6 @@
+sudo service puppet stop
+
+sudo mv /usr/lib/ruby/vendor_ruby/puppet/ /usr/lib/ruby/vendor_ruby/puppet.old/
+sudo ln -s /code/puppetlabs/puppet/lib/puppet/ /usr/lib/ruby/vendor_ruby/puppet
+sudo mv /usr/lib/ruby/vendor_ruby/puppet.rb /usr/lib/ruby/vendor_ruby/puppet.rb.old
+sudo ln -s /code/puppetlabs/puppet/lib/puppet.rb /usr/lib/ruby/vendor_ruby/puppet.rb
