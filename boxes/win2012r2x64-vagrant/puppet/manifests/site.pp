82be5cc2a583ce2d213e2cb381500768a469c9bd
Rob Reynolds <ferventcoder@gmail.com>
2014-01-09 15:09:23 -0600

    Win2012R2 jenkins "-" PUP-1389 setup

--- boxes/win2012r2x64-vagrant/puppet/manifests/site.pp
+++ boxes/win2012r2x64-vagrant/puppet/manifests/site.pp
@@ -4,120 +4,31 @@
 #   source => 'c:\vagrant\resources\packages;http://chocolatey.org/api/v2/',
 # }
 
+$slave_home = "c:/Bob/jenkins-slave"
 
-file { 'c:/temp/setup_env.bat':
-  ensure => file,
-  provider => windows,
-  #owner => 'Administrators',
-  #group => 'SYSTEM',
-  content => template("c:/vagrant/puppet/manifests/templates/setup_env.bat.erb"),
-}
-
-exec { 'setup_env.bat':
-  command => 'c:/temp/setup_env.bat',
-  #cwd => 'c:\temp',
-  #path => 'c:\temp;%PATH%',
-  provider => windows,
-  require => File['c:/temp/setup_env.bat'],
-}
-
-file {'c:/temp/moded':
+file { ["c:/Bob",$slave_home]:
   ensure => directory,
-  owner => 'Administrators',
-  group => 'SYSTEM',
-  mode => 0770,
 }
 
-file {'c:/temp/moded_vagrant':
-  ensure => directory,
-  owner => 'Administrators',
-  group => 'vagrant',
-  mode => 0770,
-}
-
-file {'c:/temp/moded.txt':
+file { "${slave_home}/jenkins-slave.exe":
   ensure => file,
-  owner => 'Administrators',
-  group => 'SYSTEM',
-  mode => 0770,
-  content => 'ah',
+  mode => '0777',
+  source => 'puppet:///modules/jenkins/jenkins-slave.exe',
+  source_permissions => ignore,
+  require => File[$slave_home],
+  backup => false,
 }
 
-# file {'c:/temp/moded.txt':
+# file { "${slave_home}/jenkins-slave.xml":
 #   ensure => file,
-#   group => 'bob',
-#   #mode => 0770,
-#   content => 'ah',
+#   content => template('jenkins/jenkins-slave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
 # }
 
-file {'c:/temp':
-  ensure => directory,
-}
-
-file {'c:/temp/moded_vagrant.txt':
-  ensure => file,
-  owner => 'Administrators',
-  group => 'vagrant',
-  mode => 0770,
-  content => 'ah',
-}
-
-file {'c:/temp/nomode':
-  ensure => directory,
-  owner => 'Administrators',
-  group => 'SYSTEM',
-}
-
-file {'c:/temp/nomode.txt':
-  ensure => file,
-  owner => 'Administrators',
-  group => 'SYSTEM',
-  content => 'wat?'
-}
-
-user {'bob':
-  ensure => present,
-  password => 'bob',
-  groups => ['Users']
-}
-
-file {'c:/temp/nomode_bob_group.txt':
-  ensure => file,
-  group => 'bob',
-  content => 'wat?',
-  require => User['bob'],
-}
-
-file {'c:/temp/nomode_bob_owner.txt':
-  ensure => file,
-  owner => 'bob',
-  content => 'wat?',
-  require => User['bob'],
-}
-
-file {'c:/temp/nomode_bob_both.txt':
-  ensure => file,
-  owner => 'bob',
-  group => 'bob',
-  content => 'wat?',
-  require => User['bob'],
-}
-
-file {'c:/temp/nomode_bob_group':
-  ensure => directory,
-  group => 'bob',
-  require => User['bob'],
-}
-
-file {'c:/temp/nomode_bob_owner':
-  ensure => directory,
-  owner => 'bob',
-  require => User['bob'],
-}
-
-file {'c:/temp/nomode_bob_both':
-  ensure => directory,
-  owner => 'bob',
-  group => 'bob',
-  require => User['bob'],
-}
+# file { "${slave_home}/jenkins.slave.exe.config":
+#   ensure => file,
+#   content => template('jenkins/jenkins.slave.exe.config.erb'),
+#   require => File[$slave_home],
+#   backup => false,
+# }
