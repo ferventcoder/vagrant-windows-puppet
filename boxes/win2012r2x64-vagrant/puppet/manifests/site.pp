a1fa39fecc371a7c701dd8cb6de477cf5b2095ab
Rob Reynolds <ferventcoder@gmail.com>
2014-01-10 16:54:09 -0600

    recreation of current issue

--- boxes/win2012r2x64-vagrant/puppet/manifests/site.pp
+++ boxes/win2012r2x64-vagrant/puppet/manifests/site.pp
@@ -4,24 +4,57 @@
 #   source => 'c:\vagrant\resources\packages;http://chocolatey.org/api/v2/',
 # }
 
-$slave_home = "c:/Bob/jenkins-slave"
+$slave_home = "c:/Bob/jenkins-slave2"
 
 file { ["c:/Bob",$slave_home]:
   ensure => directory,
 }
 
-file { "${slave_home}/jenkins-slave.exe":
+# file { "${slave_home}/jenkins-slave.exe":
+#   ensure => file,
+#   mode => '0777',
+#   source => 'puppet:///modules/jenkins/jenkins-slave.exe',
+#   source_permissions => ignore,
+#   require => File[$slave_home],
+#   backup => false,
+# }
+
+# works?
+# file { "c:\\Bob\\jenkinsslave\\jenkins-slave.xml":
+#   ensure => file,
+#   content => template('jenkins/jenkins-slave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
+# }
+
+# ReplaceFileW results in no go
+file { "${slave_home}/jenkins-slave.xml":
   ensure => file,
-  mode => '0777',
-  source => 'puppet:///modules/jenkins/jenkins-slave.exe',
-  source_permissions => ignore,
-  require => File[$slave_home],
+  content => template('jenkins/jenkins-slave.xml.erb'),
+  require => File[ $slave_home ],
   backup => false,
 }
 
-# file { "${slave_home}/jenkins-slave.xml":
+# works
+# file { "${slave_home}/jenkins-slavexml":
 #   ensure => file,
 #   content => template('jenkins/jenkins-slave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
+# }
+
+# works
+# file { "${slave_home}/jenkinsslave.xml":
+#   ensure => file,
+#   content => template('jenkins/jenkinsslave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
+# }
+
+# works
+# file { "${slave_home}/jenkins_slave.xml":
+#   ensure => file,
+#   content => template('jenkins/jenkins_slave.xml.erb'),
 #   require => File[ $slave_home ],
 #   backup => false,
 # }
