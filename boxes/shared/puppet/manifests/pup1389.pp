f5c3d0ae00352f764d24ad45d9601c6a43758d0b
Rob Reynolds <ferventcoder@gmail.com>
2014-01-14 12:43:38 -0600

    Adding recreation scripts for PUP-1389

---
+++ boxes/shared/puppet/manifests/pup1389.pp
@@ -0,0 +1,62 @@
+#$slave_home = "${::systemdrive}\\jenkins-slave"
+$slave_home = "c:\\jenkins-slave"
+
+file { $slave_home :
+  ensure => directory,
+}
+
+# file { "${slave_home}/jenkins-slave.exe":
+#   ensure => file,
+#   mode => '0777',
+#   source => 'puppet:///modules/pup1389/jenkins-slave.exe',
+#   source_permissions => ignore,
+#   require => File[$slave_home],
+#   backup => false,
+# }
+
+# ReplaceFileW results in no go
+file { "${slave_home}\\jenkins-slave.xml":
+  ensure => file,
+  content => template('pup1389/jenkins-slave.xml.erb'),
+  require => File[ $slave_home ],
+  backup => false,
+}
+
+# works?
+# file { "c:\\Bob\\jenkinsslave\\jenkins-slave.xml":
+#   ensure => file,
+#   content => template('pup1389/jenkins-slave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
+# }
+
+# works
+# file { "${slave_home}/jenkins-slavexml":
+#   ensure => file,
+#   content => template('pup1389/jenkins-slave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
+# }
+
+# works
+# file { "${slave_home}/jenkinsslave.xml":
+#   ensure => file,
+#   content => template('pup1389/jenkinsslave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
+# }
+
+# works
+# file { "${slave_home}/jenkins_slave.xml":
+#   ensure => file,
+#   content => template('pup1389/jenkins_slave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
+# }
+
+file { "${slave_home}\\jenkins-slave.exe.config":
+  ensure => file,
+  content => template('pup1389/jenkins-slave.exe.config.erb'),
+  require => File[$slave_home],
+  backup => false,
+}
--- boxes/shared/shell/puppet.run.cmd
+++ boxes/shared/shell/puppet.run.cmd
@@ -1,3 +1,10 @@
 @echo off
 
-bundle exec puppet apply --debug --trace --verbose --modulepath "C:/vagrant/puppet/modules;C:/ProgramData/PuppetLabs/puppet/etc/modules" "C:\vagrant\puppet\manifests\site.pp"
+SET FACTER_domain='local'
+SET FACTER_kernel='windows'
+SET FACTER_operatingsystem='windows'
+SET FACTER_osfamily='windows'
+
+bundle exec puppet apply --debug --trace --verbose --modulepath 'C:/ProgramData/PuppetLabs/puppet/etc/modules;C:/vagrantshared/puppet/modules;C:/vagrant/puppet/modules' --manifestdir 'C:/vagrantshared/puppet/manifests' --detailed-exitcodes 'C:/vagrantshared/puppet/manifests/pup1389.pp'
+
+exit /b %ERRORLEVEL%
