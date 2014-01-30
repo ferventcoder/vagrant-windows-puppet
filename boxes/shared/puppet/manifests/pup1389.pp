1e3e6653796e1de9685a884e3b8688075be743ad
Rob Reynolds <ferventcoder@gmail.com>
2014-01-30 15:29:52 -0600

    (maint) updating pup1389.pp to just repro file resource for pup1450

--- boxes/shared/puppet/manifests/pup1389.pp
+++ boxes/shared/puppet/manifests/pup1389.pp
@@ -5,50 +5,51 @@ file { $slave_home :
   ensure => directory,
 }
 
-# file { "${slave_home}/jenkins-slave.exe":
-#   ensure => file,
-#   mode => '0777',
-#   source => 'puppet:///modules/pup1389/jenkins-slave.exe',
-#   source_permissions => ignore,
-#   require => File[$slave_home],
-#   backup => false,
-# }
-
-# ReplaceFileW results in no go
-file { "${slave_home}\\jenkins-slave.xml":
+file { "${slave_home}/jenkins-slave.exe":
   ensure => file,
-  content => template('pup1389/jenkins-slave.xml.erb'),
-  require => File[ $slave_home ],
-  backup => false,
-}
-
-# works
-file { "${slave_home}/jenkins-slavexml":
-  ensure => file,
-  content => template('pup1389/jenkins-slave.xml.erb'),
-  require => File[ $slave_home ],
-  backup => false,
-}
-
-# works
-file { "${slave_home}/jenkinsslave.xml":
-  ensure => file,
-  content => template('pup1389/jenkins-slave.xml.erb'),
-  require => File[ $slave_home ],
-  backup => false,
-}
-
-# works
-file { "${slave_home}/jenkins_slave.xml":
-  ensure => file,
-  content => template('pup1389/jenkins-slave.xml.erb'),
-  require => File[ $slave_home ],
-  backup => false,
-}
-
-file { "${slave_home}\\jenkins-slave.exe.config":
-  ensure => file,
-  content => template('pup1389/jenkins-slave.exe.config.erb'),
+  mode => '0777',
+  #source => 'puppet:///modules/pup1389/jenkins-slave.exe',
+  source => 'C:\vagrantshared\puppet\modules\pup1389\files\jenkins-slave.exe',
+  source_permissions => ignore,
   require => File[$slave_home],
   backup => false,
 }
+
+# # ReplaceFileW results in no go
+# file { "${slave_home}\\jenkins-slave.xml":
+#   ensure => file,
+#   content => template('pup1389/jenkins-slave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
+# }
+
+# # works
+# file { "${slave_home}/jenkins-slavexml":
+#   ensure => file,
+#   content => template('pup1389/jenkins-slave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
+# }
+
+# # works
+# file { "${slave_home}/jenkinsslave.xml":
+#   ensure => file,
+#   content => template('pup1389/jenkins-slave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
+# }
+
+# # works
+# file { "${slave_home}/jenkins_slave.xml":
+#   ensure => file,
+#   content => template('pup1389/jenkins-slave.xml.erb'),
+#   require => File[ $slave_home ],
+#   backup => false,
+# }
+
+# file { "${slave_home}\\jenkins-slave.exe.config":
+#   ensure => file,
+#   content => template('pup1389/jenkins-slave.exe.config.erb'),
+#   require => File[$slave_home],
+#   backup => false,
+# }
