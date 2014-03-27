546ea9bcfd6862ec64f9797c47540dcfac905a0c
Rob Reynolds <ferventcoder@gmail.com>
2014-03-27 14:49:07 -0500

    CentOS - fix replace puppet with Dev

--- boxes/centos-6.4-x64/shell/ReplacePuppetWithDev.sh
+++ boxes/centos-6.4-x64/shell/ReplacePuppetWithDev.sh
@@ -1,7 +1,9 @@
 sudo service puppetmaster stop
 
-# mv /usr/lib/ruby/site_ruby/1.8/puppet/ /usr/lib/ruby/site_ruby/1.8/puppet.old/
-sudo mv /usr/lib/ruby/site_ruby/1.9.1/puppet/ /usr/lib/ruby/site_ruby/1.9.1/puppet.old/
-sudo ln -s /code/puppetlabs/puppet/lib/puppet/ /usr/lib/ruby/site_ruby/1.9.1/puppet
-sudo mv /usr/lib/ruby/site_ruby/puppet.rb /usr/lib/ruby/site_ruby/puppet.rb.old
-sudo ln -s /code/puppetlabs/puppet/lib/puppet.rb /usr/lib/ruby/site_ruby/1.9.1/puppet.rb
+#$ruby_ver=1.8
+#$ruby_ver=1.9.1
+
+sudo mv /usr/lib/ruby/site_ruby/1.8/puppet/ /usr/lib/ruby/site_ruby/1.8/puppet.old/
+sudo ln -s /code/puppetlabs/puppet/lib/puppet/ /usr/lib/ruby/site_ruby/1.8/puppet
+sudo mv /usr/lib/ruby/site_ruby/1.8/puppet.rb /usr/lib/ruby/site_ruby/1.8/puppet.rb.old
+sudo ln -s /code/puppetlabs/puppet/lib/puppet.rb /usr/lib/ruby/site_ruby/1.8/puppet.rb
