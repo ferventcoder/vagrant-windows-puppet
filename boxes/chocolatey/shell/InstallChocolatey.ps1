9141764a8c6d8ace9bfbc917f6c7d87114ed085a
Rob Reynolds <ferventcoder@gmail.com>
2014-07-03 09:17:17 -0500

    (chocolatey) update the install doc not to set the machine environment variable
     - chocolatey install now does this automatically

--- boxes/chocolatey/shell/InstallChocolatey.ps1
+++ boxes/chocolatey/shell/InstallChocolatey.ps1
@@ -6,28 +6,28 @@ $DebugPreference = "Continue";
 $env:ChocolateyEnvironmentDebug = 'true'
 
 # Put chocolatey on the MACHINE path, vagrant does not have access to user environment variables
-$envPath = $env:PATH
-if (!$envPath.ToLower().Contains($ChocoInstallPath.ToLower())) {
+# $envPath = $env:PATH
+# if (!$envPath.ToLower().Contains($ChocoInstallPath.ToLower())) {
 
-  Write-Host "PATH environment variable does not have `'$ChocoInstallPath`' in it. Adding..."
-  $ActualPath = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine)
-  $StatementTerminator = ";"
-  $HasStatementTerminator = $ActualPath -ne $null -and $ActualPath.EndsWith($StatementTerminator)
-  If (!$HasStatementTerminator -and $ActualPath -ne $null) {$ChocoInstallPath = $StatementTerminator + $ChocoInstallPath}
-  if (!$ChocoInstallPath.EndsWith($StatementTerminator)) {$ChocoInstallPath += $StatementTerminator}
+#   Write-Host "PATH environment variable does not have `'$ChocoInstallPath`' in it. Adding..."
+#   $ActualPath = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine)
+#   $StatementTerminator = ";"
+#   $HasStatementTerminator = $ActualPath -ne $null -and $ActualPath.EndsWith($StatementTerminator)
+#   If (!$HasStatementTerminator -and $ActualPath -ne $null) {$ChocoInstallPath = $StatementTerminator + $ChocoInstallPath}
+#   if (!$ChocoInstallPath.EndsWith($StatementTerminator)) {$ChocoInstallPath += $StatementTerminator}
 
-  [Environment]::SetEnvironmentVariable('Path', $ActualPath + $ChocoInstallPath, [System.EnvironmentVariableTarget]::Machine)
-}
+#   [Environment]::SetEnvironmentVariable('Path', $ActualPath + $ChocoInstallPath, [System.EnvironmentVariableTarget]::Machine)
+# }
 
 $env:Path += ";$ChocoInstallPath;$ChocoInstallPathOld"
 
 if (!(Test-Path $ChocoInstallPath)) {
   $resourcesPath = 'c:\vagrant\resources'
-  $chocoPkgFolder = get-childitem $resourcesPath -recurse -include 'chocolatey.*' | select -First 1
+  $chocoPkgFolder = get-childitem $resourcesPath -recurse -include 'chocolatey.*' | ?{$_.PSIsContainer} | sort-object -property Name -descending | select -First 1
 
   if ($chocoPkgFolder -ne $null) {
     # Run a completely offline install if everything is downloaded
-    Write-Host "Installing chocolatey on this machine"
+    Write-Host "Installing chocolatey on this machine from `'$chocoPkgFolder`'"
     $toolsFolder = Join-Path $chocoPkgFolder "tools"
     $chocInstallPS1 = Join-Path $toolsFolder "chocolateyInstall.ps1"
 
--- boxes/shared/shell/InstallChocolatey.ps1
+++ boxes/shared/shell/InstallChocolatey.ps1
@@ -1,27 +1,28 @@
-$ChocoInstallPath = "$env:SystemDrive\Chocolatey\bin"
+$ChocoInstallPathOld = "$env:SystemDrive\Chocolatey\bin"
+$ChocoInstallPath = "$env:SystemDrive\ProgramData\Chocolatey\bin"
 
 # Put chocolatey on the MACHINE path, vagrant does not have access to user environment variables
-$envPath = $env:PATH
-if (!$envPath.ToLower().Contains($ChocoInstallPath.ToLower())) {
+# $envPath = $env:PATH
+# if (!$envPath.ToLower().Contains($ChocoInstallPath.ToLower())) {
 
-  Write-Host "PATH environment variable does not have `'$ChocoInstallPath`' in it. Adding..."
-  $ActualPath = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine)
-  $StatementTerminator = ";"
-  $HasStatementTerminator = $ActualPath -ne $null -and $ActualPath.EndsWith($StatementTerminator)
-  If (!$HasStatementTerminator -and $ActualPath -ne $null) {$ChocoInstallPath = $StatementTerminator + $ChocoInstallPath}
-  if (!$ChocoInstallPath.EndsWith($StatementTerminator)) {$ChocoInstallPath += $StatementTerminator}
+#   Write-Host "PATH environment variable does not have `'$ChocoInstallPath`' in it. Adding..."
+#   $ActualPath = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine)
+#   $StatementTerminator = ";"
+#   $HasStatementTerminator = $ActualPath -ne $null -and $ActualPath.EndsWith($StatementTerminator)
+#   If (!$HasStatementTerminator -and $ActualPath -ne $null) {$ChocoInstallPath = $StatementTerminator + $ChocoInstallPath}
+#   if (!$ChocoInstallPath.EndsWith($StatementTerminator)) {$ChocoInstallPath += $StatementTerminator}
 
-  [Environment]::SetEnvironmentVariable('Path', $ActualPath + $ChocoInstallPath, [System.EnvironmentVariableTarget]::Machine)
-}
+#   [Environment]::SetEnvironmentVariable('Path', $ActualPath + $ChocoInstallPath, [System.EnvironmentVariableTarget]::Machine)
+# }
 
-$env:Path += ";$ChocoInstallPath"
+# $env:Path += ";$ChocoInstallPath"
 
 if (!(Test-Path $ChocoInstallPath)) {
   # Install chocolatey
   iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))
 }
 
-choco install chocolatey -pre
+#choco install chocolatey -pre
 # $resourcesPath = 'c:\vagrantshared\resources'
 # $chocoPkgFile = get-childitem $resourcesPath -recurse -include 'chocolatey.*.nupkg' | select -First 1
 
