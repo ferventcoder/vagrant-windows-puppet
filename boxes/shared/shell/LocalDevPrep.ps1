61ac44a06289ac461b2cc914b713fe0d3cd32237
Rob Reynolds <ferventcoder@gmail.com>
2014-08-26 12:54:50 -0500

    (maint) don't install gem docs

--- boxes/shared/shell/LocalDevPrep.ps1
+++ boxes/shared/shell/LocalDevPrep.ps1
@@ -5,12 +5,12 @@ write-host "This should be run from the root of your puppet code base"
 choco install ruby -version 1.9.3.54500
 $env:PATH += ";$env:SystemDrive\ruby193\bin"
 choco install ruby.devkit
-&gem install bundler
+&gem install --no-ri --no-rdoc bundler
 &bundler config --global jobs 4
-&gem install debug_inspector -v '0.0.2'
+&gem install --no-ri --no-rdoc debug_inspector -v '0.0.2'
 #&gem install debugger-ruby_core_source -v '1.2.3'
-&gem install debugger
-&gem install debugger -v '1.6.2'
+&gem install --no-ri --no-rdoc debugger
+&gem install --no-ri --no-rdoc debugger -v '1.6.2'
 &bundle install
 
 &bundle exec puppet apply -e "user {'Administrator': ensure=>present, groups=>['Administrators'],}"
