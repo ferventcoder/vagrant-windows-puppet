56edff938c52c8c76e256def9a38905cda716b6e
Rob Reynolds <ferventcoder@gmail.com>
2015-08-06 14:15:48 -0500

    (maint) fix os powershell stream issues in prep

--- boxes/shared/shell/PrepareWindows.ps1
+++ boxes/shared/shell/PrepareWindows.ps1
@@ -8,21 +8,21 @@ function Add-PuppetMasterToHostsFile {
   #Add-Content $env:SystemDrive\Windows\System32\Drivers\etc\hosts "`r`n192.168.0.4     puppet"
 }
 
-Write-Host 'Checking if puppet server has been added to hosts file'
+#Write-Host 'Checking if puppet server has been added to hosts file'
 try {
   Test-Connection puppet -ErrorAction Stop
 } catch {
   Add-PuppetMasterToHostsFile
 }
 
-Write-Host 'Ensuring Time Service is on'
-net start w32time
-w32tm /resync
+#Write-Host 'Ensuring Time Service is on'
+cmd.exe /c "net start w32time  2>&1"
+cmd.exe /c "w32tm /resync  2>&1"
 
-Write-Host 'Ensuring TCP/IP NetBIOS Helper Service (lmhosts) is on'
-cmd.exe /c "sc config lmhosts start= auto"
-net start lmhosts
+#Write-Host 'Ensuring TCP/IP NetBIOS Helper Service (lmhosts) is on'
+cmd.exe /c "sc config lmhosts start= auto 2>&1"
+cmd.exe /c "net start lmhosts 2>&1"
 
-Write-Host "Ensuring certificates are installed if box isn't updated"
+#Write-Host "Ensuring certificates are installed if box isn't updated"
 certutil -v -addstore Root "c:\vagrantshared\resources\certs\geotrust.global.pem"
 certutil -v -addstore Root "c:\vagrantshared\resources\certs\usertrust.network.pem"
