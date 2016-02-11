95eec71109e1ac6e879a085e7dc46fa7ae1a9660
Rob Reynolds <ferventcoder@gmail.com>
2016-02-11 12:45:16 -0600

    (shared) update choco provider tests

--- boxes/shared/puppet/manifests/chocolatey_provider_tests.pp
+++ boxes/shared/puppet/manifests/chocolatey_provider_tests.pp
@@ -6,16 +6,39 @@ case $operatingsystem {
   }
 }
 
-include chocolatey
+#include chocolatey
+
+# class {'chocolatey':
+#   log_output              => true,
+# }
+class {'chocolatey':
+  chocolatey_download_url => 'file:///c:/code/temp/chocolatey.0.9.9.10.nupkg',
+  use_7zip                => false,
+  log_output              => true,
+}
+#include chocolatey_server
+
+
+# chocolatey_source {'local':
+#   ensure    => 'present',
+#   location  => 'http://localhost/chocolatey'
+# }
 
 package {'roundhouse':
-  ensure   => '0.8.5.0',
+  ensure   => '0.8.6',
 }
 
-package {'putty':
-  ensure   => installed,
-}
+#->
 
-package {'launchy':
-  ensure   => latest,
-}
+# package {'putty':
+#   ensure   => installed,
+# } ->
+
+# package {'launchy':
+#   ensure   => latest,
+# }
+# ->
+
+# chocolatey::source {'chocolatey':
+#   enable => false,
+# }
